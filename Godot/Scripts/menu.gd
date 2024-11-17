2bb5321c8c132e921c2587cca23589773335f87f
Oscar Ryley <oscar.ryley@gmail.com>
2024-11-17 11:26:27 +0000

    Implemented Button Designs for Main Menu

--- Godot/Scripts/menu.gd
+++ Godot/Scripts/menu.gd
@@ -3,8 +3,5 @@ extends Control
 func _on_play_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/connections_screen.tscn")
 
-func _on_shroom_gallery_pressed() -> void:
-	pass # Replace with function body.
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
