97c0ac89fdb8b4b2b680df679047aceba556183d
KaalkidanSahele <kaalkidan@hotmail.com>
2024-11-17 11:05:23 +0000

    back button

--- Godot/Scripts/points_display.gd
+++ Godot/Scripts/points_display.gd
@@ -2,3 +2,7 @@ extends RichTextLabel
 
 func _process(delta: float) -> void:
 	text = str(Global.points)
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
